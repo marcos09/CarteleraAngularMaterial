6a58ed2da5a017f9b4ae5e432329e1e22ea59c10
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2017-08-16 01:17:18 -0300

    setup material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import {MdSliderModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MdSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]

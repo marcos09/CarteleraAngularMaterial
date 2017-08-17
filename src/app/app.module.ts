8031c7cd4a5a078e32696def17a21d8cef67076c
Johana Taus <yato_t@live.com.ar>
2017-08-17 12:11:41 -0300

    inicio con barra y grid list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,43 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 import { AppComponent } from './app.component';
 import {MdSliderModule} from '@angular/material';
+import {MdGridListModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+import {MdMenuModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
+import {MdInputModule} from '@angular/material';
 
+import {Component} from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MdSliderModule
+    BrowserModule, BrowserAnimationsModule, MdSliderModule, MdGridListModule, MdToolbarModule, MdIconModule, MdButtonModule, MdMenuModule, MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+@Component({
+  selector: 'grid-list-overview-example'
+})
+export class GridListOverviewExample {}
+
+@Component({
+  selector: 'toolbar-multirow-example'
+})
+export class ToolbarMultirowExample {}
+
+@Component({
+  selector: 'menu-icons-example'
+})
+export class MenuIconsExample {}
+
+@Component({
+  selector: 'input-prefix-suffix-example'
+})
+export class InputPrefixSuffixExample { }
